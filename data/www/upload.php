014602e6dc3e91353e5949b5f2d6bc08fe127c58
Simon <ymwsimon@gmail.com>
2026-01-19 13:01:31 +0000

    php stuff

--- data/www/upload.php
+++ data/www/upload.php
@@ -1,17 +1,35 @@
 <?php
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+// $uploadOk = 1;
+// $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
+
+$dir = "./upload/";
+
+// echo "\n";
+// echo $_FILES["fileToUpload"]["tmp_name"];
+// echo "\n";
+// echo $_FILES["fileToUpload"]["name"];
+// echo "\n";
+if (!file_exists($dir))
+  mkdir($dir, 0700);
+if (!empty($_FILES["fileToUpload"]["tmp_name"]) && file_exists($dir) && is_writeable($dir))
+  {
+    move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $dir . $_FILES["fileToUpload"]["name"]);
+    echo "file uploaded\n";
   }
-  else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
+else
+  echo "file size too large or upload folder is not writeable\n";
+// if(isset($_POST["submit"])) {
+//   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+//   if($check !== false) {
+//     echo "File is an image - " . $check["mime"] . ".";
+//     $uploadOk = 1;
+//   }
+//   else {
+//     echo "File is not an image.";
+//     $uploadOk = 0;
+//   }
+// }
+
 ?>                        
